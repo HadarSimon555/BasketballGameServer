9d904718fd5345f8c6b5ddddb8338e85a8a2721a
User <User@LAB2-2>
2021-10-03 09:11:50 +0300

    חיבור הקונטרולר לבסיס הנתונים

---
+++ BasketballGameServer/Controllers/BasketballGameController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BasketballGameServerBL.Models;
+using System.IO;
+
+namespace BasketballGameServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketballGameController : ControllerBase
+    {
+        #region Add connection to the db context using dependency injection
+        BasketballGameDBContext context;
+        public BasketballGameController(BasketballGameDBContext context)
+        {
+            this.context = context;
+        }
+        #endregion
+    }
+}
