09c33bed14c839c895c3cd35ae05f1224c747630
User <User@LAB2-2>
2021-10-10 09:32:18 +0300

    עדכון הסקיפט

--- BasketballGameServer/DataBaseScripts/BasketballGameDB.sql
+++ BasketballGameServer/DataBaseScripts/BasketballGameDB.sql
@@ -1,15 +1,142 @@
-USE [master]
-GO
-
-/****** Object:  Database [BasketballGameDB]    Script Date: 19/09/2021 08:52:07 ******/
-CREATE DATABASE [BasketballGameDB]
- 
-Use BasketballGameDB
-Go
-
-Create Table Users (
-ID int Identity primary key,
-Email nvarchar(100) not null UNIQUE,
-Pswd nvarchar(30) not null UNIQUE,
-)
-Go
+CREATE TABLE "Player"(
+    "id" INT NOT NULL,
+    "height" FLOAT NOT NULL,
+    "birthDate" DATETIME NOT NULL,
+    "name" NVARCHAR(255) NOT NULL,
+    "image" NVARCHAR(255) NULL
+);
+ALTER TABLE
+    "Player" ADD CONSTRAINT "player_id_primary" PRIMARY KEY("id");
+CREATE TABLE "Team"(
+    "id" INT NOT NULL,
+    "coachId" INT NOT NULL,
+    "leagueId" INT NOT NULL,
+    "name" NVARCHAR(255) NOT NULL,
+    "image" NVARCHAR(255) NULL
+);
+ALTER TABLE
+    "Team" ADD CONSTRAINT "team_id_primary" PRIMARY KEY("id");
+CREATE TABLE "PlayerOnTeamForSeason"(
+    "positionId" INT NOT NULL,
+    "playerId" INT NOT NULL,
+    "teamId" INT NOT NULL,
+    "seasonId" INT NOT NULL
+);
+ALTER TABLE
+    "PlayerOnTeamForSeason" ADD CONSTRAINT "playeronteamforseason_positionid_primary" PRIMARY KEY("positionId");
+CREATE TABLE "Position"(
+    "id" INT NOT NULL,
+    "name" NVARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "Position" ADD CONSTRAINT "position_id_primary" PRIMARY KEY("id");
+CREATE TABLE "Season"(
+    "id" INT NOT NULL,
+    "name" NVARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "Season" ADD CONSTRAINT "season_id_primary" PRIMARY KEY("id");
+CREATE TABLE "Game"(
+    "id" INT NOT NULL,
+    "seasonId" INT NOT NULL,
+    "homeTeamId" INT NOT NULL,
+    "awayTeamId" INT NOT NULL,
+    "gameStatusId" INT NOT NULL,
+    "scoreAwayTeam" INT NOT NULL,
+    "scoreHomeTeam" INT NOT NULL
+);
+ALTER TABLE
+    "Game" ADD CONSTRAINT "game_id_primary" PRIMARY KEY("id");
+CREATE TABLE "GameStats"(
+    "id" INT NOT NULL,
+    "gameId" INT NOT NULL,
+    "throw2Pts" INT NOT NULL,
+    "sling2Pts" INT NOT NULL,
+    "throw3Pts" INT NOT NULL,
+    "sling3Pts" INT NOT NULL,
+    "playerId" INT NOT NULL
+);
+ALTER TABLE
+    "GameStats" ADD CONSTRAINT "gamestats_id_primary" PRIMARY KEY("id");
+CREATE TABLE "RequestGame"(
+    "id" INT NOT NULL,
+    "requestStatusId" INT NOT NULL,
+    "coachId" INT NOT NULL,
+    "gameId" INT NOT NULL
+);
+ALTER TABLE
+    "RequestGame" ADD CONSTRAINT "requestgame_id_primary" PRIMARY KEY("id");
+CREATE TABLE "Coach"(
+    "id" INT NOT NULL,
+    "name" NVARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "Coach" ADD CONSTRAINT "coach_id_primary" PRIMARY KEY("id");
+CREATE TABLE "RequestStatus"(
+    "id" INT NOT NULL,
+    "name" NVARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "RequestStatus" ADD CONSTRAINT "requeststatus_id_primary" PRIMARY KEY("id");
+CREATE TABLE "League"(
+    "id" INT NOT NULL,
+    "name" NVARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "League" ADD CONSTRAINT "league_id_primary" PRIMARY KEY("id");
+CREATE TABLE "GameStatus"(
+    "id" INT NOT NULL,
+    "name" NVARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "GameStatus" ADD CONSTRAINT "gamestatus_id_primary" PRIMARY KEY("id");
+CREATE TABLE "RequestToJoinTeam"(
+    "id" INT NOT NULL,
+    "playerId" INT NOT NULL,
+    "teamId" INT NOT NULL
+);
+ALTER TABLE
+    "RequestToJoinTeam" ADD CONSTRAINT "requesttojointeam_id_primary" PRIMARY KEY("id");
+CREATE TABLE "User"(
+    "id" INT NOT NULL,
+    "email" NVARCHAR(255) NOT NULL,
+    "pass" NVARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "User" ADD CONSTRAINT "user_id_primary" PRIMARY KEY("id");
+CREATE UNIQUE INDEX "user_email_unique" ON
+    "User"("email");
+CREATE UNIQUE INDEX "user_pass_unique" ON
+    "User"("pass");
+ALTER TABLE
+    "RequestToJoinTeam" ADD CONSTRAINT "requesttojointeam_playerid_foreign" FOREIGN KEY("playerId") REFERENCES "Player"("id");
+ALTER TABLE
+    "PlayerOnTeamForSeason" ADD CONSTRAINT "playeronteamforseason_playerid_foreign" FOREIGN KEY("playerId") REFERENCES "Player"("id");
+ALTER TABLE
+    "GameStats" ADD CONSTRAINT "gamestats_playerid_foreign" FOREIGN KEY("playerId") REFERENCES "Player"("id");
+ALTER TABLE
+    "RequestToJoinTeam" ADD CONSTRAINT "requesttojointeam_teamid_foreign" FOREIGN KEY("teamId") REFERENCES "Team"("id");
+ALTER TABLE
+    "Game" ADD CONSTRAINT "game_awayteamid_foreign" FOREIGN KEY("awayTeamId") REFERENCES "Team"("id");
+ALTER TABLE
+    "Game" ADD CONSTRAINT "game_hometeamid_foreign" FOREIGN KEY("homeTeamId") REFERENCES "Team"("id");
+ALTER TABLE
+    "PlayerOnTeamForSeason" ADD CONSTRAINT "playeronteamforseason_teamid_foreign" FOREIGN KEY("teamId") REFERENCES "Team"("id");
+ALTER TABLE
+    "PlayerOnTeamForSeason" ADD CONSTRAINT "playeronteamforseason_seasonid_foreign" FOREIGN KEY("seasonId") REFERENCES "Season"("id");
+ALTER TABLE
+    "Game" ADD CONSTRAINT "game_seasonid_foreign" FOREIGN KEY("seasonId") REFERENCES "Season"("id");
+ALTER TABLE
+    "RequestGame" ADD CONSTRAINT "requestgame_gameid_foreign" FOREIGN KEY("gameId") REFERENCES "Game"("id");
+ALTER TABLE
+    "GameStats" ADD CONSTRAINT "gamestats_gameid_foreign" FOREIGN KEY("gameId") REFERENCES "Game"("id");
+ALTER TABLE
+    "Team" ADD CONSTRAINT "team_coachid_foreign" FOREIGN KEY("coachId") REFERENCES "Coach"("id");
+ALTER TABLE
+    "RequestGame" ADD CONSTRAINT "requestgame_coachid_foreign" FOREIGN KEY("coachId") REFERENCES "Coach"("id");
+ALTER TABLE
+    "RequestGame" ADD CONSTRAINT "requestgame_requeststatusid_foreign" FOREIGN KEY("requestStatusId") REFERENCES "RequestStatus"("id");
+ALTER TABLE
+    "Team" ADD CONSTRAINT "team_leagueid_foreign" FOREIGN KEY("leagueId") REFERENCES "League"("id");
+ALTER TABLE
+    "Game" ADD CONSTRAINT "game_gamestatusid_foreign" FOREIGN KEY("gameStatusId") REFERENCES "GameStatus"("id");
