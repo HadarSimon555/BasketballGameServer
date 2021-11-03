a65149a623b119f3f4732338f59767c21e8c4207
User <User@LAB2-2>
2021-11-03 13:45:06 +0200

    3.11

--- BasketballGameServerBL/ModelsBL/BasketballGameDBContext.cs
+++ BasketballGameServerBL/ModelsBL/BasketballGameDBContext.cs
@@ -10,12 +10,15 @@ namespace BasketballGameServerBL.Models
 {
     public partial class BasketballGameDBContext:DbContext
     {
+        #region Login
         // פעולת התחברות, אם ההתחברות לא הצליחה מחזיר null
         public User Login(string email, string pass)
         {
             return this.Users.Where(u => u.Email == email && u.Pass == pass).FirstOrDefault();
         }
+        #endregion
 
+        #region AddPlayer
         public void AddPlayer(Player player)
         {
             try
@@ -28,7 +31,9 @@ namespace BasketballGameServerBL.Models
                 Console.WriteLine(e.Message);
             }
         }
+        #endregion
 
+        #region AddCoach
         public void AddCoach(Coach coach)
         {
             try
@@ -40,6 +45,19 @@ namespace BasketballGameServerBL.Models
             {
                 Console.WriteLine(e.Message);
             }
+        }
+        #endregion
+
+        // פעולה הבודקת האם האימייל שהתקבל הוא ייחודי או שהוא כבר קיים ברשימת המשתמשים
+        public bool UserExistByEmail(string email)
+        {
+            return Users.Any(u => u.Email == email);
+        }
+
+        // פעולה הבודקת האם הסיסמה שהתקבלה היא ייחודית או שהיא כבר קיים ברשימת המשתמשים
+        public bool UserExistByUserName(string pass)
+        {
+            return Users.Any(u => u.Pass == pass);
         }
     }
 }
